73ba2c79a8aa37934b14dbb54230d223e325f3c5
Anthony <anthonyn2121@gmail.com>
2024-08-16 06:50:53 -0400

    no tree created when test running

--- node.py
+++ node.py
@@ -15,10 +15,10 @@ class Node:
         return np.array([self.x, self.y, self.z])
 
     def __add__(self, rhs):
-        return Node(self.x + rhs.x, self.y + rhs.y, self.z + rhs.y)
+        return Node(self.x + rhs.x, self.y + rhs.y, self.z + rhs.z)
     
     def __sub__(self, rhs):
-        return Node(self.x - rhs.x, self.y - rhs.y, self.z - rhs.y)
+        return Node(self.x - rhs.x, self.y - rhs.y, self.z - rhs.z)
     
     def __mul__(self, rhs):
         if (not isinstance(rhs, Node)):
@@ -27,19 +27,23 @@ class Node:
         else:
             return NotImplementedError("No vector multiplication needed")
 
-
-
     def __rmul__(self, rhs):
         return self * rhs
-    
+
+    def __abs__(self):
+        return Node(abs(self.x), abs(self.y), abs(self.z))
+
     @staticmethod
     def normalize(node):
         x, y, z = node.x, node.y, node.z
-        magnitude = np.sqrt(x**2 + y**2 + z**2)
+        magnitude = (x**2 + y**2 + z**2)**(0.5)
         return Node(x/magnitude, y/magnitude, z/magnitude)
 
     def __str__(self):
-        return f"[{self.x}, {self.y}, {self.z}]"
+        return self.__repr__
+
+    def __repr__(self):
+        return f"[{self.x}, {self.y}, {self.z}] parent={self.parent} cost = {self.cost}"
 
 
 if __name__ == "__main__":
--- rrt.py
+++ rrt.py
@@ -3,38 +3,10 @@ import json
 import numpy as np
 from environment_toolkit.environment import Environment
 from environment_toolkit.occupancy_map import OccupancyMap
+from node import Node
 
 # https://arxiv.org/pdf/1105.1186
 # https://roboticsproceedings.org/rss06/p34.pdf
-class Node:
-    def __init__(self, x, y, z, parent=None):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.parent = parent
-
-    def copy(self):
-        return Node(self.x, self.y, self.z, self.parent)
-
-    def to_array(self):
-        return np.array([self.x, self.y, self.z])
-
-    def __add__(self, rhs):
-        return Node(self.x + rhs.x, self.y + rhs.y, self.z + rhs.y)
-    
-    def __sub__(self, rhs):
-        return Node(self.x - rhs.x, self.y - rhs.y, self.z - rhs.y)
-    
-    def __mul__(self, rhs):
-        # scalar multiplication
-        return Node(rhs*self.x, rhs*self.y, rhs*self.z)
-    
-    @staticmethod
-    def normalize(node):
-        x, y, z = node.x, node.y, node.z
-        magnitude = np.sqrt(x**2 + y**2 + z**2)
-        return Node(x/magnitude, y/magnitude, z/magnitude)
-
 def distance(from_node, to_node):
     dx = to_node.x - from_node.x
     dy = to_node.y - from_node.y
@@ -49,26 +21,52 @@ class RRT:
         self.nodes = []
         self.edges = set()
 
-    def plan(self, start, goal, max_iters=100):
+    def plan(self, start, goal, max_iters=100, goal_radius=3.0):
         ## Start a vertex set with only the initial point and and empty edge set
         self.nodes = [Node(start[0], start[1], start[2])]
+        goal_node = Node(goal[0], goal[1], goal[2])
+        best_goal_node = None
         for _ in range(max_iters):
             ## Randomly select a point in free space
             sampled_node = self.__random_sample()
             ## Find nearest vertex
             nearest_node = self.__get_closest_node(sampled_node)
             ## Find the a point on the line from the nearest_node to the sampled_node within a step size
-            new_node = self.__steer(nearest_node, sampled_node, step_size=1.0)
+            new_node = self.__steer(nearest_node, sampled_node, step_size=2.0)
 
             if (self.__is_collision_free(nearest_node, new_node)):
-                ## Find nearest nodes
-                pass
-                
-                # RADIUS = 3.0
-                # cost_map = {node: cost(new_node, node) for node in self.nodes}
-                # nearest = dict(filter(lambda item: item[1] < RADIUS, cost_map.items()))  ## Get key-value pairs where value < RADIUS
-            
+                new_node.parent = nearest_node
+                new_node.cost = nearest_node.cost + distance(nearest_node, new_node)
 
+                ## Find nearest nodes
+                RADIUS = 3.0
+                nearest_nodes = [node for node in self.nodes if distance(node, new_node) < RADIUS]
+
+                ## Rewire nodes
+                for node in nearest_nodes:
+                    new_cost = new_node.cost + distance(new_node, node)
+                    print("self.__is_collision_free(node, new_node) and (new_cost < node.cost):")
+                    print(self.__is_collision_free(node, new_node) and (new_cost < node.cost))
+                    if (self.__is_collision_free(node, new_node) and (new_cost < node.cost)):
+                        node.parent = new_node
+                        node.cost = new_cost
+                        print(node)
+
+                ## Check if connection to goal is valid
+                if distance(new_node, goal_node) < goal_radius and\
+                   self.__is_collision_free(new_node, goal_node):
+                    potential_goal_node = Node(goal[0], goal[1], goal[2],
+                                               parent = new_node,
+                                               cost = new_node.cost + distance(new_node, goal_node))
+
+                    if (best_goal_node is None) or (potential_goal_node.cost < best_goal_node.cost):
+                        best_goal_node = potential_goal_node
+
+        if (best_goal_node):
+            return self.__trace_path(best_goal_node)
+        else:
+            return None
+        
     def __random_sample(self) -> Node:
         x_min, x_max, y_min, y_max, z_min, z_max = self.world.map_bounds
 
@@ -86,7 +84,7 @@ class RRT:
             z = self.resolution * round(z/self.resolution)
 
             node = Node(x, y, z)
-            if self.occupancy.is_valid_position(node):
+            if self.occupancy.is_valid_position(node.to_array()):
                 valid_node = True
 
         return node
@@ -95,14 +93,18 @@ class RRT:
         '''
         Get the closest node in the tree
         '''
-        cost_map = {node:distance(node, sample_node) for node in self.node}
-        return min(cost_map, key=cost_map.get())
+        cost_map = {node:distance(node, sample_node) for node in self.nodes}
+        return min(cost_map, key=cost_map.get)
     
     def __steer(self, nearest_node:Node, sampled_node:Node, step_size=1.0) -> Node:
         '''
         steer the nearest node towards the sampled node given the step size
         '''
-        v = nearest_node - sampled_node ## difference between vectors
+        
+        # d = abs(distance(nearest_node, sampled_node))
+        if (step_size > distance(nearest_node, sampled_node)):
+            return AssertionError("Step size of __steer needs to be less than the distance between points")
+        v = abs(nearest_node - sampled_node) ## difference between vectors
         return nearest_node + (step_size * Node.normalize(v))
 
     def __is_collision_free(self, from_node, to_node) -> bool:
@@ -114,23 +116,31 @@ class RRT:
                 return False
         return True
 
-
+    def __trace_path(self, best_goal_node:Node) -> np.array:
+        path = []
+        current_node = best_goal_node
+        while current_node:
+            path.append(current_node.to_array())
+            current_node = current_node.parent
+        return path[::-1]
+    
 if __name__ == "__main__":
     import argparse
-   
-    world_data = '''{
-        "bounds": [0, 10, 0, 10, 0, 10],
-        "blocks": [
-            {"position":[1, 1, 1], "size": [2, 2, 2]},
-            {"position":[5, 5, 0], "size": [3, 3, 3]},
-            {"position":[7, 2, 4], "size": [1, 4, 1]}
-            ],
-        "start": [4, 2, 0],
-        "goal": [9, 8, 8],
-        "margin": 0.5,
-        "resolution": 0.5
-        }'''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--file', '-f', default="environment_toolkit/worlds/test_simple.json", help="json file that describes an environment")
+    args = parser.parse_args()
 
-    world = json.loads(world_data)
+
+    world_filepath = args.file
+    with open(world_filepath, 'r') as file:
+        world = json.load(file)
+
     world_data = {key:world[key] for key in ["bounds", "blocks"]}
+    env = Environment(world_data)
+    start = tuple(world['start'])
+    goal = tuple(world['goal'])
+    margin = world['margin']
+    resolution = world['resolution']
 
+    rrt = RRT(env, resolution)
+    print(rrt.plan(start, goal, max_iters=1))
